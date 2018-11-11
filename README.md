6eeb6ede1fe698f3311443496c3c1cdef0642dbd
Your Name <a.szuflicki@gmail.com>
2018-11-11 12:48:59 +0100

    Restarting again

--- README.md
+++
@@ -1,2 +0,0 @@
-# Eowyn
-# Eowyn
--- front/src/components/LandingPage.actions.js
+++
@@ -1,8 +0,0 @@
-export const UPDATE_PRICE = "UPDATE_PRICE";
-export const updatePrice = (price) => {
-    
-    return {
-        type: UPDATE_PRICE,
-        payload: price
-    }
-}
