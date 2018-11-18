54a7b5fa46631899bdbf8d0e7a93ae7861a1c603
Your Name <a.szuflicki@gmail.com>
2018-11-18 14:56:15 +0100

    Prices DB initialize script corrected

--- PricesDB/init/init.sql
+++ PricesDB/init/init.sql
@@ -1,10 +1,24 @@
 CREATE DATABASE prices;
-CREATE USER pricessService WITH ENCRYPTED PASSWORD 'pricessService';
-GRANT ALL PRIVILEGES ON DATABASE prices TO pricessService;
+CREATE USER priceservice WITH ENCRYPTED PASSWORD 'priceservice123';
+GRANT ALL PRIVILEGES ON DATABASE prices TO priceservice;
 
-CREATE TABLE btc(
-  _time TIMESTAMP NOT NULL,
-  price DOUBLE PRECISION
-);
 
-SELECT create_hypertable('btc', '_time', if_not_exists => TRUE);
+\connect prices;
+
+CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
+
+CREATE TABLE IF NOT EXISTS "btcs" (
+  _time TIMESTAMP WITH TIME ZONE NOT NULL, 
+  price DOUBLE PRECISION,
+  PRIMARY KEY (_time)
+  );
+
+GRANT ALL PRIVILEGES ON TABLE btcs TO priceservice;
+
+SELECT create_hypertable('"btcs"', '_time');
+
+insert into btcs (_time, price) values ('2018-11-18 12:35:06', 12);
+
+-- select * from btc;
+
+
