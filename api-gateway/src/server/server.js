79865046ade563b80e0304d974e3a8ef5f8a9a25
Your Name <a.szuflicki@gmail.com>
2018-11-24 21:04:53 +0100

    Merge

--- api-gateway/src/server/server.js
+++ api-gateway/src/server/server.js
@@ -2,18 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const proxy = require('http-proxy-middleware')
-<<<<<<< HEAD
-const start = (options) => {
-	return new Promise((resolve, reject) => {
-		if (!options.routes) {
-			reject(new Error('The server must be started with a discovered routes'))
-=======
 
 const start = (options) => {
 	return new Promise((resolve, reject) => {
 		if (!options.routes) {
 			reject(new Error('The server must be started with a list of routes'))
->>>>>>> 6e979d5ddd8ea5f5484a50ebc16ee50c76ce796c
 		}
 		if (!options.port) {
 			reject(new Error('The server must be started with an available port'))
