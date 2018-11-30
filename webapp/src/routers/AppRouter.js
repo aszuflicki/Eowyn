c211c4185ed4d630eaee1c570a8265cb86387677
Your Name <a.szuflicki@gmail.com>
2018-11-30 21:37:19 +0100

    Sidebar added

--- webapp/src/routers/AppRouter.js
+++ webapp/src/routers/AppRouter.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory'
-// import PrivateRoute from './PrivateRoute';
+import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
-import Chart from '../components/Chart/Index.component'
 import Navbar from '../components/Navbar.component'
-import Sidebar from '../components/Sidebar.component'
 
 import LandingPage from '../components/LandingPage.component'
-import Dashboard from '../components/Dashboard.component'
+import Dashboard from '../components/Dashboard/Dashboard.component'
 
 export const history = createHistory();
 
@@ -16,9 +14,9 @@ const AppRouter = () => (
     <React.Fragment>
         <Router history={history}>
             <Switch>
-                <Route path="/" exact={true} component={Chart} />
-                {/* <PublicRoute path="/" exact={true} component={LandingPage} /> */}
-                {/* <PublicRoute path="/dashboard" component={Dashboard} /> */}
+                {/* <Route path="/" exact={true} component={LandingPage} /> */}
+                <PublicRoute path="/" exact={true} component={LandingPage} />
+                <PublicRoute path="/dashboard" component={Dashboard} />
 
             </Switch>
         </Router>
