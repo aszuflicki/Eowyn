977448a51669fa87832be670f6527fea326b29ca
Your Name <a.szuflicki@gmail.com>
2019-01-10 18:35:11 +0100

    docker network

--- nodeapp/start.sh
+++ nodeapp/start.sh
@@ -2,7 +2,7 @@
 
 docker rm   nodeapp  --force
 
-docker run -p 8081:8081 --name nodeapp -d nodeapp
+docker run -p 8081:8081 --network eowyn-net --name nodeapp -d nodeapp
 
 docker logs -t -f nodeapp
 
